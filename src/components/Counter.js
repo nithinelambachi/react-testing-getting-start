aed1821f688830fb3aa9759a16a38d95c4a9a118
dfeverx <dfeverx@gmail.com>
2022-03-12 12:08:44 +0530

    counter decrement

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,17 @@
 import { useState } from 'react'
-
+import './Counter.css'
 export default function Counter() {
     const [count, setCount] = useState(0)
     function handleIncrement() {
         setCount(count + 1)
     }
+    function handleDecrement() {
+        setCount(count - 1)
+    }
     return (
-        <div><h1>{count}</h1>
+        <div className='container'><h1>{count}</h1>
             <button onClick={handleIncrement}>Increment</button>
+            <button onClick={handleDecrement}>Decrement</button>
         </div>
     )
 }
